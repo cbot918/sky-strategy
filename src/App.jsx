4225382355155f9954bfde503e4bd0274379b61b
cbot918 <cbot918@gmail.com>
2023-01-21 17:13:47 +0800

    update: basic layout and bottom button

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container h-full flex flex-col ">
+        <div className="top flex justify-center items-center h-16 border-solid border-2 text-blue-600">
+          top
+        </div>
+        <div className="main flex justify-center items-center h-96 border-solid border-2">
+          main
+        </div>
+        <div className="bottom p-5 flex justify-between items-center h-16 border-solid border-2">
+          <div className="btn1">抽</div>
+          <div className="btn2">城</div>
+          <div className="btn3">副</div>
+          <div className="btn4">野</div>
+          <div className="btn5">包</div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
